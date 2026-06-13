e2fbf741581b7088ad7e32d0a0cc0fc65214ccd5
BM1964-25 <bernhard.metzger@me.com>
2026-06-13 15:30:12 +0200

    Make Teams migration robust

--- supabase/migrations/005_add_teams_meeting_location.sql
+++ supabase/migrations/005_add_teams_meeting_location.sql
@@ -1,4 +1,7 @@
 alter table bookings
+  add column if not exists meeting_location text not null default 'phone';
+
+alter table bookings
   drop constraint if exists bookings_meeting_location_check;
 
 alter table bookings
