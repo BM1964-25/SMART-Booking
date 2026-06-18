9ffc064e788bfdd0fd878bc5d3324c154901d466
BM1964-25 <bernhard.metzger@me.com>
2026-06-18 18:12:23 +0200

    Add smartbooking landing page

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/smartbooking",
+        destination: "/smartbooking/index.html",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
